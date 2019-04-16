5f7d296138d9e90636fb11e166e711dadbb96b2c
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-04-16 10:46:07 +1000

    Add files via upload

---
+++ MyBayesianSentimentModel.R
@@ -0,0 +1,31 @@
+library(RTextTools)
+library(e1071) #SVM
+
+pos_tweets =  rbind(
+  c('I love this car', 'positive'),
+  c('This view is amazing', 'positive'),
+  c('I feel great this morning', 'positive'),
+  c('I am so excited about the concert', 'positive'),
+  c('He is my best friend', 'positive')
+)
+
+neg_tweets = rbind(
+  c('I do not like this car', 'negative'),
+  c('This view is horrible', 'negative'),
+  c('I feel tired this morning', 'negative'),
+  c('I am not looking forward to the concert', 'negative'),
+  c('He is my enemy', 'negative')
+)
+
+test_tweets = rbind(
+  c('feel happy this morning', 'positive'),
+  c('larry friend', 'positive'),
+  c('not like that man', 'negative'),
+  c('house not great', 'negative'),
+  c('your song annoying', 'negative')
+)
+
+# build dtm
+matrix= create_matrix(tweets[,1], language="english", 
+                      removeStopwords=FALSE, removeNumbers=TRUE, 
+                      stemWords=FALSE) 
