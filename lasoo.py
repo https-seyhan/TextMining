b93445f750a7f2ffad64175c4224a8c9d9a4cf92
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-04-16 10:48:23 +1000

    Add files via upload

---
+++ lasoo.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import random
+#import tkinter
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+
+def test():
+	print('Test')
+	%matplotlib inline
+
+rcParams['figure.figsize'] = 12, 10
+
+#Define input array with angles from 60deg to 300deg converted to radians
+x = np.array([i*np.pi/180 for i in range(60,300,4)])
+np.random.seed(10)  #Setting seed for reproducability
+y = np.sin(x) + np.random.normal(0,0.15,len(x))
+data = pd.DataFrame(np.column_stack([x,y]),columns=['x','y'])
+plt.plot(data['x'],data['y'],'.')
+
+if __name__ == '__main__':
+	test()
