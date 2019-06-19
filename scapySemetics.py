c49d5f43cc87285ce44ef55347e77613dabbdacf
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-06-19 23:58:38 +1000

    Add files via upload

--- scapySemetics.py
+++ scapySemetics.py
@@ -5,10 +5,20 @@ Spyder Editor
 This is a temporary script file.
 """
 import spacy
+from spacy.lang.en import English
+from spacy.pipeline import EntityRuler
 
-nlp = spacy.load('en_core_web_md')
-tokens = nlp(u'car toyota milk')
-
-for token in tokens:
-    print(token.text, token.has_vector, token.vector_norm, token.is_oov)
+nlp = spacy.load('en_core_web_md')  # make sure to use larger model!
+ruler = EntityRuler(nlp)
+patterns = [{"label": "education", "pattern": "book"}]
+ruler.add_patterns(patterns)
+nlp.add_pipe(ruler)
+tokens = nlp(u'car travel education other')
+mytoken = nlp('book')
+#mytoken = nlp('booking')
+#mytoken = nlp('toyota')
+for token1 in mytoken:
+    
+    for token2 in tokens:
+        print((token1.text, token1.label_), token2.text, token1.similarity(token2))
     
