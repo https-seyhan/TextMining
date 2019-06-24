482db87fdec8599487bfc5c55dee516ae1c21f3a
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-06-24 23:33:28 +1000

    Add files via upload

---
+++ gloVeModel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 22:57:51 2019
+
+@author: saul
+"""
+
+from glove import Corpus, Glove
+
+# creating a corpus object
+corpus = Corpus() 
+
+#training the corpus to generate the co occurence matrix which is used in GloVe
+corpus.fit(lines, window=10)
+
+#creating a Glove object which will use the matrix created in the above lines to create embeddings
+#We can set the learning rate as it uses Gradient Descent and number of components
+
+glove = Glove(no_components=5, learning_rate=0.05)
+ 
+glove.fit(corpus.matrix, epochs=30, no_threads=4, verbose=True)
+glove.add_dictionary(corpus.dictionary)
+glove.save('glove.model')
