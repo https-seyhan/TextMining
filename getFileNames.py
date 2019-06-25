ca5aa460474b581b5c32972d3c4d1b23e806dc1f
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-06-25 22:18:00 +1000

    Add files via upload

---
+++ getFileNames.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 21:59:39 2019
+
+@author: saul
+"""
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+import os
+os.chdir('/home/saul/pythontraining')
+
+simple_list=[['','']]
+onlyfiles = [f for f in listdir('/home/saul/pythontraining') if isfile(join('/home/saul/pythontraining', f))]
+for line in onlyfiles:
+    Type = line.split(".")
+    print(Type[0])
+    print(Type[1])
+    simple_list.append([Type[0], Type[1]])
+		
+df=pd.DataFrame(simple_list,columns=['fileName','extension'])	
+	
+df.to_csv('filedetails.csv', sep=',', index=False)
