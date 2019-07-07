333f4302d8aa3c8bdc0b226faa1d091a040490e5
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-07-08 00:14:47 +1000

    Add files via upload

---
+++ itemWork_5.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 00:12:42 2019
+
+@author: saul
+"""
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[69]:
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 00:46:57 2019
+
+@author: saul
+"""
+
+import plac
+from spacy.lang.en import English
+from spacy.matcher import PhraseMatcher, Matcher
+from spacy.tokens import Doc, Span, Token
+import spacy
+import pandas as pd
+import re
+
+nlp = spacy.load("en_core_web_sm")
+
+customize_stop_words = [
+    'From', 'To'
+]
+for w in customize_stop_words:
+    nlp.vocab[w].is_stop = True
+
+
+def getItem(receipt):
+     
+    
+    text = """
+    "Net income was $9.4 million compared to the prior year of $2.7 million.",
+    "Revenue exceeded twelve billion dollars, with a loss of $1b. run",
+    """
+    text = receipt.strip("n\n")
+    text = receipt.strip("\\n")
+    
+    text = re.sub(r"[\'?'\n\'[]:]","",text, flags=re.I)
+    #text = re.sub("\n","", text)
+    #text.replace(r"['\n']", '')
+    
+    #text = ', '.join(text)
+    #text = text.replace('"', '')
+    #text = text.replace('\n', '')
+    #print (mylist)
+
+    
+    #print("Text ", type(text))
+    #print("Text :", text)
+    doc = nlp(text)
+#print("Tokens", [t.text for t in doc])
+
+#Every word in the text is a token
+#print(doc.text[0])
+#print(doc.text[1])
+#print("Tags", [(t.text, t.tag_, t.pos_) for t in doc],'\n\n')
+#print("Tokens", [t.text for t in doc],'\n')
+ 
+
+#remove stop wods and verbs
+    cleantext = [t.text for t in doc if not t.is_stop and t.pos_ == 'NOUN']
+    print("Clean Text ", cleantext) 
+
+#cleandoc = nlp(Doc(nlp.vocab, cleantext))
+# convert list ot nlp doc
+    cleandoc = Doc(nlp.vocab, words=cleantext)
+    #print("cleandoc ", cleandoc)
+    matcher = Matcher(nlp.vocab)
+# Add match ID "item" with no callback and one pattern
+    pattern1 = [{"LOWER": "compared"}]
+    pattern2 = [{"LOWER": "description"}]
+    pattern3 = [{"LOWER": "prior"}]
+    pattern4 = [{"LOWER": "receipt"}]
+    matcher.add("item", None, pattern1, pattern2, pattern3, pattern4)
+    matches = matcher(cleandoc)
+
+    word_list = []
+    word_dict = {}
+    print(matches)
+    for match_id, start, end in matches:
+        string_id = nlp.vocab.strings[match_id]  # Get string representation
+        span = cleandoc[start:end]  # The matched span
+        print(start, end, span.text)
+        word_dict[span.text] = {} # create dictionary for keyword
+        word_dict[span.text][cleandoc[start-1]] = -1
+
+    #print(doc[start-1])
+        word_list.append(cleandoc[start-1])
+    #print(doc[start-2])
+    #word_dict[span.text].([cleandoc[start-2]])
+        word_dict[span.text][cleandoc[start-2]] = -2
+                                
+        word_list.append(cleandoc[start-2])
+    #print(doc[start-3])
+        word_dict[span.text][cleandoc[start-3]] = -3
+    
+        word_list.append(cleandoc[start-3])
+    #print(doc[start-4])
+        word_dict[span.text][cleandoc[start-4]] = -4
+        word_list.append(cleandoc[start-4])
+    
+    #print(doc[start-5],'\n')
+        word_dict[span.text][cleandoc[start-5]] = -5
+        word_list.append(cleandoc[start-5])
+    #print(doc[start+1])
+        word_dict[span.text][cleandoc[start+1]] = 1
+        word_list.append(cleandoc[start + 1])
+    #print(doc[start+2])
+        word_dict[span.text][cleandoc[start+2]] = 2
+        word_list.append(cleandoc[start + 2])
+    #print(doc[start+3])
+        word_dict[span.text][cleandoc[start+3]] = 3
+        word_list.append(cleandoc[start + 3])
+    #print(doc[start+4])
+    
+        word_dict[span.text][cleandoc[start+4]] = 4
+        word_list.append(cleandoc[start + 4])
+    #print(doc[start+5],'\n')
+        word_dict[span.text][cleandoc[start+5]] = 5
+        word_list.append(cleandoc[start + 5])
+        print(word_list)
+        print(word_dict)
+        
+def getText():
+    text2 = pd.read_csv('/home/saul/pythontraining/NLP/imagetotext.csv')
+    
+    #get a line 
+    mytext = text2.iloc[[1]]
+    receipt = mytext.iloc[0].text
+    #print(receipt)
+    #print(type(receipt))
+    getItem(receipt)
+        
+
+getText()
+    #getItem()
+
+
+# In[57]:
+
+
+mylist=['"Newyork"\n', '"Teacher2"', '"A"']
+mylist = ', '.join(mylist)
+mylist = mylist.replace('"', '')
+mylist = mylist.replace('\n', '')
+print (mylist)
+
+
+
+# In[6]:
+
+
+print(type(receipt))
+
+
+# In[ ]:
+
+
+
