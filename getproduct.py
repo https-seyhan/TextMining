4b58c4ce2bf10d462761c9b51e8477c55d66eae0
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-07-09 00:17:51 +1000

    Add files via upload

---
+++ getproduct.py
@@ -0,0 +1,7 @@
+import spacy
+nlp = spacy.load("en_core_web_lg")
+text = "iPhones."
+doc = nlp(text)
+for ent in doc.ents:
+    if ent.label_ == "PRODUCT":
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
