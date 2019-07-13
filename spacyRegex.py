aa7391b65a375220c301ac6de4fa1827c7d7e410
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-07-13 14:15:32 +1000

    Add files via upload

--- spacyRegex.py
+++ spacyRegex.py
@@ -7,20 +7,17 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 text = """
-    "Net income was $9.4 million acompared to the prior year of $2.7 million.",
-    "Revenue exceeded twelve billion dollars, with a loss of $1b. run",
+"Net income was $9.4 million acompared to the prior year of $2.7
+million.",
+"Revenue exceeded twelve billion dollars, with a loss of $1b. run",
 """
+
 doc = nlp(text)
 
-pattern1 = [{"LOWER": "compared"}]
-pattern = [{"LOWER": {"REGEX": "\b\w*compared\w*\b"}}]
-#pattern3 = [{"LOWER": {"REGEX": "compared}}]
+pattern = [{"LOWER": {"REGEX": "\\b\w*(?:compared)\w*\b"}}]
 
 matcher = Matcher(nlp.vocab)
-matcher.add("item", None,  pattern )
+matcher.add("item", None, pattern )
 matches = matcher(doc)
 print(matches)
 print(matcher)
-
-import re
-re.findall("united", "123united")
