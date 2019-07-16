6ddc440b327d839cdec249edb1c6b9bfc52c2b9e
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-07-17 00:17:50 +1000

    Add files via upload

---
+++ pdftext.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#import pdfminer.six
+#import pdfminer3k
+#import pdfminer
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+#import pdfminer.six
+from pdfminer.pdfpage import PDFPage
+import os
+import sys, getopt
+
+os.chdir('/home/saul/pdfwork')
+path = 'sample.pdf'
+
+
+print("x")
+
+def convert_pdf_to_txt(path):
+    print(path)
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
+    print("FP :", fp)
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, 
+                                  password=password,
+                                  caching=caching, 
+                                  check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+def convert(fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+ 
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+ 
+    infile = open(fname, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+        print(interpreter.process_page(page))
+    print(converter)
+    infile.close()
+    #print(output)
+    converter.close()
+    text = output.getvalue()
+    output.close
+    #print(text)
+    
+    #Save pdf file to text
+    with open('texted.txt','w') as f:
+        f.write(text)
+    
+    return text 
+
+
+if __name__ == '__main__':
+     #convert_pdf_to_txt(path)
+     convert('sample.pdf')
+    
