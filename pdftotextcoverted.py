c86c4cd04808284c86b1b10bdda69c62640629b8
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-07-17 01:00:14 +1000

    Add files via upload

---
+++ pdftotextcoverted.py
@@ -0,0 +1,19 @@
+
+import PyPDF2
+import os
+import glob
+
+os.chdir('/home/saul/pythontraining/NLP')
+print(glob.glob("*.pdf"))
+
+pdfFileObj = open('GassBill2.pdf','rb')     
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+
+pageObj = pdfReader.getPage(0)          #'9' is the page number
+pageObj.extractText()
+
+from tika import parser
+
+raw = parser.from_file('GassBill2.pdf')
+print(raw['content'])
