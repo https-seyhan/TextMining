14cacd59b6495690debfbdfffcb0c4bb784e4509
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <sauldata@tutanota.com>
2020-07-09 10:11:37 +1000

    Add files via upload

---
+++ NLP/NLPWork.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 29 20:41:02 2019
+
+@author: saul
+"""
+
+import pandas as pd
+import os
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB 
+
+os.chdir('/home/saul/pythonWork/NLP/Chapter_5/Data')
+vec = CountVectorizer(min_df =0.001, max_df=0.95)
+
+report = pd.read_csv('prReports.csv', encoding='utf-8')
+print(len(report.columns))
+print(report.columns)
+
+clf = MultinomialNB()
+
+X = report['Description:'].values
+print(X[0])
+y= report['Warning:']
+print(pd.crosstab(index=report['Warning:'],columns='count'))
