8bd3e138c18d399bc28ffab4ca9c310e930d1b6d
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-05 09:30:23 +1100

    Update Fan.R

--- Fan.R
+++ Fan.R
@@ -139,8 +139,6 @@ wordcloud(words = d.users$word, freq = d.users$freq, min.freq = 1,
           max.words=200, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
 
-
-
 ####Office365 Negative Posts 
 office365negativeTalks <- office365negative$body.post
 summary(office365negativeTalks)
