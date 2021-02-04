1c1f6c8dc94ecbe6e1962c6b586042f8a08c68bb
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-05 10:34:01 +1100

    Update Fan.R

--- Fan.R
+++ Fan.R
@@ -518,7 +518,6 @@ df <- data.frame(V1 = posts)
 
 
 mycorpus <- Corpus(DataframeSource(df))
-
 mycorpus <- tm_map(mycorpus, removePunctuation)
 mycorpus <- tm_map(mycorpus, removeNumbers)
 mycorpus <- tm_map(mycorpus , removeWords, c("also", "you", "would", "youll", "your", "youre", "https",
