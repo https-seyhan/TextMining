5f222a3c41ac0dd9fd8205d0d9864dc72f2cccf0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-05 10:09:47 +1100

    Update Fan.R

--- Fan.R
+++ Fan.R
@@ -416,8 +416,6 @@ users.df <- data.frame(V1 = windowsnegativeUsers )
 users.corpus <- Corpus(DataframeSource(users.df))
 users.corpus <- tm_map(users.corpus, removePunctuation)
 users.corpus <- tm_map(users.corpus, removeNumbers)
-
-
 users.corpus <- tm_map(users.corpus , removeWords, c("and","also", "you", "would", "youll", "your", "youre", "https",
                                                      "httpst","httpstc","httpstco", "come", "this", "the", "will", "are", "that", "with", "amp", "all", "from", "have",
                                                      "ampamp","please", "our","more", "for", "you", "wont", "within", "without", "with", "were", "which", "office", "any",
