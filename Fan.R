bd4aca937d243d54679bc619568606cff610f3b5
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-05 09:29:51 +1100

    Update Fan.R

--- Fan.R
+++ Fan.R
@@ -117,7 +117,6 @@ summary(office365negativeUsers)
 
 users.df <- data.frame(V1 = office365negativeUsers)
 users.corpus <- Corpus(DataframeSource(users.df))
-
 users.corpus <- tm_map(users.corpus, removePunctuation)
 users.corpus <- tm_map(users.corpus, removeNumbers)
 users.corpus <- tm_map(users.corpus , removeWords, c("and","also", "you", "would", "youll", "your", "youre", "https",
