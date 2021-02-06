a06f877abdf4f83b85158672f6d5be6f65c7caee
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-06 18:08:33 +1100

    Update NLP_Analysis_Microsoft_Products.R

--- NLP_Analysis_Microsoft_Products.R
+++ NLP_Analysis_Microsoft_Products.R
@@ -541,7 +541,6 @@ mycorpus <- tm_map(mycorpus , removeWords, c("also", "you", "would", "youll", "y
                                              "httpst","httpstc","httpstco", "come", "this", "the", "will", "are", "that", "with", "amp", "all", "from", "have",
                                              "ampamp","please", "our","more", "for", "you", "wont", "within", "without", "with", "were", "which", "office", "any",
                                              "fuck", "fucking", "and", "was", "didnt", "can", "why", "and"))
-
 corpus <- Corpus(VectorSource(mycorpus))
 
 tdm <- TermDocumentMatrix(corpus)
