1c42259fe07c39fc23db7cf33ce2e4a9e433170f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-21 20:59:58 +1000

    Update OCRonImages.py

--- OCR/OCRonImages.py
+++ OCR/OCRonImages.py
@@ -27,7 +27,6 @@ for file in onlyfiles:
     fileReader = PyPDF2.PdfFileReader(open(file,'rb')) # read pdf files in the directory
 
     count = 0
-
     while count < 3: # read first 2 pages of the pdf files
         pageObj = fileReader.getPage(count)
         count +=1
