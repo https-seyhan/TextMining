83a2415d7759e27c74ef116f2f6bd586f53cbd29
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-29 22:04:33 +1000

    Update directoryWalk_OCR.py

--- OCR/directoryWalk_OCR.py
+++ OCR/directoryWalk_OCR.py
@@ -76,7 +76,7 @@ def convertImageString(folders):
 def list_files(dir):                                                                                                  
     r = []                                                                                                           
     subdirs = [x[0] for x in os.walk(dir)]                                                                            
-    for subdir in subdirs:                                                                                            
+    for subdir in subdirs:                                                                                           
         files = next(os.walk(subdir))[2]                                                                             
         if (len(files) > 0):                                                                                          
             for file in files:                                                                                       
