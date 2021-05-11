033c1fde69742afabd73f3a656cc5a63ba912bc3
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-11 19:38:01 +1000

    Update itemWork_5.py

--- NLP/itemWork_5.py
+++ NLP/itemWork_5.py
@@ -19,7 +19,6 @@ for w in customize_stop_words:
     nlp.vocab[w].is_stop = True
 
 def getItem(receipt):
-
     text = """
     "Net income was $9.4 million compared to the prior year of $2.7 million.",
     "Revenue exceeded twelve billion dollars, with a loss of $1b. run",
