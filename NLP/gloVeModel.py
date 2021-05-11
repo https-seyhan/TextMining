89e66b15913396137dc55d9f3f7ce9156cd28da9
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-11 19:26:46 +1000

    Update gloVeModel.py

--- NLP/gloVeModel.py
+++ NLP/gloVeModel.py
@@ -14,7 +14,7 @@ corpus.fit(lines, window=10)
 #We can set the learning rate as it uses Gradient Descent and number of components
 
 glove = Glove(no_components=5, learning_rate=0.05)
- 
+
 glove.fit(corpus.matrix, epochs=30, no_threads=4, verbose=True)
 glove.add_dictionary(corpus.dictionary)
 glove.save('glove.model')
