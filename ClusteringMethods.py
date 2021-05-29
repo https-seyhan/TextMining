98fe6c7bd6305cf0c194d9e154cc8723b03bbeac
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-30 09:50:07 +1000

    Update ClusteringMethods.py

--- ClusteringMethods.py
+++ ClusteringMethods.py
@@ -40,7 +40,7 @@ def manhattan(rating1, rating2):
 	total = 0
 	for key in rating1:
 		if key in rating2 :
-			#print(rating1[key])
+			
 			distance +=abs(rating1[key] - rating2[key])
 			total +=1
 	if total > 0:
