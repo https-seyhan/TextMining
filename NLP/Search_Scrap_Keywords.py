3f31e8383e9f26e6f925f12cc0ce62011bd7941b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-30 09:35:20 +1000

    Update Search_Scrap_Keywords.py

--- NLP/Search_Scrap_Keywords.py
+++ NLP/Search_Scrap_Keywords.py
@@ -57,7 +57,7 @@ for match_id, start, end in matches:
     word_list.append(doc[start + 5])
 print(word_list)
  
-   
+  
 #p1 = [{'LOWER': 'quickbrownfox'}]  
 #p2 = [{'LOWER': 'quick'}, {'IS_PUNCT': True}, {'LOWER': 'brown'}, {'IS_PUNCT': True}, {'LOWER': 'fox'}]  
 #p3 = [{'LOWER': 'quick'}, {'LOWER': 'brown'}, {'LOWER': 'fox'}]  
