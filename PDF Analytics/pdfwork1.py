5a85220cc83d2bea2d1574888905940d089f11bc
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-05 10:08:58 +1000

    Update pdfwork1.py

--- PDF Analytics/pdfwork1.py
+++ PDF Analytics/pdfwork1.py
@@ -37,7 +37,7 @@ def call_convert(src,dest):
     print('Finished calling convert on %s'%src)
 
 def get_pdf_diff(pdf1, cleanup=True):
-  
+ 
     #Create a difference pdf by overlaying the two pdfs and generating an image difference.Returns True if the file matches else returns false
     #Get the list of images using get_image_list_from_pdf which inturn calls convert on a given pdf  
     pdf1_list = get_image_list_from_pdf(pdf1)
