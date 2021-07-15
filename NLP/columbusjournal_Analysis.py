d33e5c64c45d61099052874f89fc6176bfc7a943
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-16 08:20:12 +1000

    Update columbusjournal_Analysis.py

--- NLP/columbusjournal_Analysis.py
+++ NLP/columbusjournal_Analysis.py
@@ -83,7 +83,7 @@ def getPurpose(model, clean_text, sentenceNum):
     words = clean_text[sentenceNum].split(" ")
     sentencepurpose = {}
     #get tfidf vectors and insert into a dictionary
-  
+ 
     for word in range(len(wordweights)): 
         print(words[word])
         sentencepurpose[words[word]] = wordweights[word]
