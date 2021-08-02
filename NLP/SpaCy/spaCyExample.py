475363f50f38d5f38e5f7d26f7333b2367373fa1
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-03 09:09:28 +1000

    Update spaCyExample.py

--- NLP/SpaCy/spaCyExample.py
+++ NLP/SpaCy/spaCyExample.py
@@ -12,7 +12,6 @@ nlp = en_core_web_sm.load()
 
 # In[3]:
 
-
 doc = nlp('European authorities fined Google tax receipt a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
 print([(X.text, X.label_) for X in doc.ents])
 
