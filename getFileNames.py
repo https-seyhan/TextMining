8d3741aead15fb9db3e8337a5fada2713ff2619d
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-16 00:37:11 +1000

    Update getFileNames.py

--- getFileNames.py
+++ getFileNames.py
@@ -14,6 +14,7 @@ from os.path import isfile, join
 os.chdir('/home/saul/pythontraining')
 simple_list=[['','']]
 onlyfiles = [f for f in listdir('/home/saul/pythontraining') if isfile(join('/home/saul/pythontraining', f))]
+
 for line in onlyfiles:
     Type = line.split(".")
     print(Type[0])
