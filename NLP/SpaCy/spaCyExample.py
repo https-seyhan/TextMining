1f21a815d03a2e7eedfe8d91ff9be3e466414535
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-21 15:04:28 +1100

    Update spaCyExample.py

--- NLP/SpaCy/spaCyExample.py
+++ NLP/SpaCy/spaCyExample.py
@@ -26,7 +26,7 @@ def url_to_string(url):
     res = requests.get(url)
     html = res.text
     soup = BeautifulSoup(html, 'html5lib')
-   
+  
     for script in soup(["script", "style", 'aside']):
         script.extract()
     return " ".join(re.split(r'[\n\t]+', soup.get_text()))
