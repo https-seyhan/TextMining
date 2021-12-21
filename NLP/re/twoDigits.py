22d5e639ef7e8e4a4e67870df059122ee5e373a4
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-21 14:50:18 +1100

    Update twoDigits.py

--- NLP/re/twoDigits.py
+++ NLP/re/twoDigits.py
@@ -2,6 +2,7 @@ import re
 s='my age is 25. I have 55.000 percent marks and 9764135408 is my number'
 
 re.findall('\d+[.]\d\d',s)
+
 if len(re.findall('\d+[.]\d\d+',s)) > 0:
     print("Two Digit Found")
 else:
