dec3459a2958ef4cf179de2765cc47be9f158e89
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-03-21 09:27:13 +1100

    Update pdfminerWork.py

--- PDF Analytics/pdfminerWork.py
+++ PDF Analytics/pdfminerWork.py
@@ -16,7 +16,7 @@ os.chdir('/home/saul/royal/royal/spiders/')
 
 def readfiles(firstFile, secondFile):
     pdfdetails = {}
-    
+   
     print("READINNG PDF FILE :",firstFile)
     pdfReader1 = PyPDF2.PdfFileReader(open(firstFile, "rb"))  # PdfFileReader object
     pdfReader2 = PyPDF2.PdfFileReader(open(secondFile, "rb"))  # PdfFileReader object
