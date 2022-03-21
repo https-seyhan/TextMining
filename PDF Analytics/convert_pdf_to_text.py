4687bbcd88a71b0ac37defd120237d816bd0cd3b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-03-22 09:37:43 +1100

    Update convert_pdf_to_text.py

--- PDF Analytics/convert_pdf_to_text.py
+++ PDF Analytics/convert_pdf_to_text.py
@@ -31,7 +31,7 @@ def get_image_list_from_pdf(pdf_file):
 def call_convert(src,dest):
     #Call convert to convert pdf to jpg
     print('About to call convert on %s'%src)
-   
+  
     try:
         subprocess.check_call(["convert",src,dest], shell=True)
     except Exception as e:
