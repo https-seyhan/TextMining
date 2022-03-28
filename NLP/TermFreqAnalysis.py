e05592d7e7a111f4d622de6fa6425fbb4c06b630
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-03-28 14:30:04 +1100

    Update TermFreqAnalysis.py

--- NLP/TermFreqAnalysis.py
+++ NLP/TermFreqAnalysis.py
@@ -22,6 +22,7 @@ stop_words = stopwords.words('english')
 #print(nltk.word_tokenize(columbus_pos))
 
 wtokens = nltk.word_tokenize(columbus_pos)
+
 #print(len(wtokens) )   # Number of words in text
 
 table = str.maketrans('', '', string.punctuation)
