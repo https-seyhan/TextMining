49a57f6cd25849da0c20f3394a1b2139e38c4369
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-03-29 12:57:03 +1100

    Update ClusteringMethods.py

--- ClusteringMethods.py
+++ ClusteringMethods.py
@@ -49,7 +49,7 @@ def manhattan(rating1, rating2):
 
 def computeNearestNeigbour(username, users):
 	distances = []
-	
+
 	for user in users:
 		if user != username:
 			#distance = manhattan(users[user], users[username])
