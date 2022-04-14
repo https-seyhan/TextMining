7dbf44968cf6a3b4c909875508671beb13172677
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-15 08:55:12 +1000

    Update parseTree.py

--- parseTree.py
+++ parseTree.py
@@ -24,7 +24,6 @@ def main(model="en_core_web_sm"):
     # The easiest way is to find the head of the subtree you want, and then use
     # the `.subtree`, `.children`, `.lefts` and `.rights` iterators. `.subtree`
     # is the one that does what you're asking for most directly:
-
     for word in doc:
         if word.dep_ in ("xcomp", "ccomp"):
             print("".join(w.text_with_ws for w in word.subtree))
@@ -33,7 +32,7 @@ def main(model="en_core_web_sm"):
     # instead of a generator over the tokens. If you want the `Span` you can
     # get it via the `.right_edge` and `.left_edge` properties. The `Span`
     # object is nice because you can easily get a vector, merge it, etc.
-  
+ 
     for word in doc:
         if word.dep_ in ("xcomp", "ccomp"):
             subtree_span = doc[word.left_edge.i : word.right_edge.i + 1]
