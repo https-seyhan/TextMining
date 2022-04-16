5e115b2ce0dee4a386cd4e8880cff76f5c665e4c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-17 09:30:19 +1000

    Update labelling.py

--- NLP/SpaCy/labelling.py
+++ NLP/SpaCy/labelling.py
@@ -29,7 +29,6 @@ def main(model="en_core_web_sm"): # load model
     
     print("Loaded model '%s'" % model)
     print("Processing %d texts" % len(TEXTS))
-
     for text in TEXTS:
         doc = nlp(text)
         relations = extract_currency_relations(doc)
