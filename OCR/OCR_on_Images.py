def3fcd3c8886bbe379c372fc510e8c446fe1a12
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-16 22:56:55 +1000

    Update OCR_on_Images.py

--- OCR/OCR_on_Images.py
+++ OCR/OCR_on_Images.py
@@ -36,4 +36,4 @@ for file in onlyfiles:
 
 def convertImageString():
     a = PI.open("first.jpg") # sample image
-    print(image_to_string(a)) # extract text in the images
+    print(image_to_string(a)) # text in the images
