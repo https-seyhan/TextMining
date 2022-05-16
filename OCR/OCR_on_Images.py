7b4c9ceb904c7e6258d7123bbdc3ff1046e0b3c9
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-16 22:55:32 +1000

    Update OCR_on_Images.py

--- OCR/OCR_on_Images.py
+++ OCR/OCR_on_Images.py
@@ -10,7 +10,7 @@
 # Scrap text of images.
 import os
 import pytesseract # Python-tesseract is an optical character recognition (OCR) tool for python. NN based.
-from pytesseract import image_to_string # convert image to text
+from pytesseract import image_to_string # convert image to text.
 import PyPDF2
 
 from PIL import Image as PI # Python image library
