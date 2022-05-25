41e26f73750608b1b894240f71b7ebf1c869b1fb
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-25 19:58:29 +1000

    Update directoryWalk_OCR.py

--- OCR/directoryWalk_OCR.py
+++ OCR/directoryWalk_OCR.py
@@ -22,7 +22,7 @@ outputpath = '/home/saul/pythontraining/NLP/'
 enlargesize = 1
 
 def convertImageString(folders):    
-    image_to_text_list = [['', '', '']] # Add folder, image names and text 
+    image_to_text_list = [['', '', '']] # Add folder, image names and text in the 
     image_list = []
     print ("Folders ", folders) #
     
